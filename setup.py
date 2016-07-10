e76f6b83e57b92964a786c7cea89e112e191e016
Pablo Huijse <pablo.huijse@gmail.com>
2016-07-10 18:03:31 -0400

    Basic structure for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'P4J',
+    packages = ['P4J'], 
+    version = '0.1',
+    description = 'Periodic light curve analysis tools based on Information Theory',
+    author = 'Pablo Huijse',
+    author_email = 'pablo.huijse@gmail.com',
+    url = 'https://github.com/phuijse/P4J', 
+    download_url = 'https://github.com/phuijse/P4J/tarball/0.1', # I'll explain this in a second
+    keywords = ['astronomy', 'periodic', 'time series', 'correntropy'], 
+    classifiers = [],
+)
