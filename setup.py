cdc3733d0bf89f0a9f5599455089e84be2c5b491
Pablo Huijse <pablo.huijse@gmail.com>
2016-07-12 17:50:43 -0400

    add python versions

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email = 'pablo.huijse@gmail.com',
     license='MIT',
     url = 'https://github.com/phuijse/P4J', 
-    download_url = 'https://github.com/phuijse/P4J/tarball/0.1', # I'll explain this in a second
+    download_url = 'https://github.com/phuijse/P4J/tarball/0.1', 
     keywords = ['astronomy periodic time series correntropy'], 
     install_requires=[
         'numpy'
@@ -26,8 +26,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Environment :: Console',
         #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.6',
         #'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
